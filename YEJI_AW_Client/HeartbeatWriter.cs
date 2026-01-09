8b303c28d06af425b429429763a7f8497fdf72ec
sseus01 <min7101@gmail.com>
2026-01-09 12:29:41 +0900

    HeartbeatWriter 클래스 추가: 주기적 heartbeat 파일 기록
    HeartbeatWriter 클래스를 새로 추가하여 지정된 파일 경로에 일정 주기(기본 30초)로 현재 UTC 시간을 ISO 8601 포맷으로 기록하는 기능을 구현하였습니다. 생성 시 상위 디렉터리가 없으면 자동 생성하며, System.Timers.Timer를 사용해 주기적으로 heartbeat를 기록합니다. 예외 발생 시 무시하며, IDisposable 구현을 통해 자원 해제가 가능합니다.

---
+++ YEJI_AW_Client/HeartbeatWriter.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Timers;
+
+public class HeartbeatWriter : IDisposable
+{
+    private readonly Timer _timer;
+    private readonly string _heartbeatFilePath;
+
+    /// <summary>
+    /// heartbeatFilePath 예: Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.CommonApplicationData), "YEJI_AW", "heartbeat", "heartbeat.txt")
+    /// intervalMs 기본 30초(30000 ms)
+    /// </summary>
+    public HeartbeatWriter(string heartbeatFilePath, double intervalMs = 30000)
+    {
+        _heartbeatFilePath = heartbeatFilePath ?? throw new ArgumentNullException(nameof(heartbeatFilePath));
+        var dir = Path.GetDirectoryName(_heartbeatFilePath);
+        if (!string.IsNullOrEmpty(dir))
+        {
+            Directory.CreateDirectory(dir);
+        }
+
+        _timer = new Timer(intervalMs);
+        _timer.Elapsed += (s, e) => WriteHeartbeat();
+        _timer.AutoReset = true;
+        _timer.Start();
+    }
+
+    private void WriteHeartbeat()
+    {
+        try
+        {
+            // UTC ISO 8601 포맷
+            File.WriteAllText(_heartbeatFilePath, DateTime.UtcNow.ToString("o"));
+        }
+        catch
+        {
+            // 실패 시 무시하거나 내부 로그 남기기(클라이언트 로그)
+        }
+    }
+
+    public void Dispose()
+    {
+        try
+        {
+            _timer?.Stop();
+            _timer?.Dispose();
+        }
+        catch { }
+    }
+}
