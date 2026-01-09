7e8ce01d0b52c9f90db49585cf4fa7d522b6fc32
sseus01 <min7101@gmail.com>
2026-01-09 15:08:02 +0900

    TrayAppContext에 YEJI_AW_Client 네임스페이스 적용
    TrayAppContext 클래스를 YEJI_AW_Client 네임스페이스로 이동하여 코드 구조를 명확히 했습니다. 기능적 변경 없이 네임스페이스 추가로 코드 관리와 범위가 개선되었습니다.

--- YEJI_AW_Client/TrayAppContext.cs
+++ YEJI_AW_Client/TrayAppContext.cs
@@ -2,66 +2,69 @@
 using System.Windows.Forms;
 using System.IO;
 
-public class TrayAppContext : ApplicationContext
+namespace YEJI_AW_Client
 {
-    private readonly NotifyIcon _trayIcon;
-    private readonly HeartbeatWriter? _heartbeatWriter;
-
-    public TrayAppContext(HeartbeatWriter? heartbeatWriter = null)
+    public class TrayAppContext : ApplicationContext
     {
-        _heartbeatWriter = heartbeatWriter;
+        private readonly NotifyIcon _trayIcon;
+        private readonly HeartbeatWriter? _heartbeatWriter;
 
-        // TODO: Replace Properties.Resources.AppIcon with actual 리소스 또는 Icon 파일 로드
-        _trayIcon = new NotifyIcon()
+        public TrayAppContext(HeartbeatWriter? heartbeatWriter = null)
         {
-            Icon = Properties.Resources.AppIcon, // 대체: new Icon(Path.Combine(AppContext.BaseDirectory, "app.ico"))
-            Text = "YEJI-ON",
-            Visible = true,
-            ContextMenuStrip = BuildContextMenu()
-        };
+            _heartbeatWriter = heartbeatWriter;
 
-        // 예외 처리 시 트레이 정리
-        Application.ApplicationExit += OnApplicationExit;
-        AppDomain.CurrentDomain.UnhandledException += (s, e) => OnApplicationExit(s, EventArgs.Empty);
-        Application.ThreadException += (s, e) => OnApplicationExit(s, EventArgs.Empty);
-        TaskScheduler.UnobservedTaskException += (s, e) => { OnApplicationExit(s, EventArgs.Empty); e.SetObserved(); };
-    }
-
-    private ContextMenuStrip BuildContextMenu()
-    {
-        var menu = new ContextMenuStrip();
-        menu.Items.Add("열기", null, (s, e) => ShowMainWindow());
-        menu.Items.Add("종료", null, (s, e) => ExitThread());
-        return menu;
-    }
-
-    private void ShowMainWindow()
-    {
-        // 필요 시 메인 윈도우를 띄우는 로직 추가
-    }
-
-    protected override void ExitThreadCore()
-    {
-        OnApplicationExit(this, EventArgs.Empty);
-        base.ExitThreadCore();
-    }
-
-    private void OnApplicationExit(object? sender, EventArgs e)
-    {
-        try
-        {
-            if (_trayIcon != null)
+            // TODO: Replace Properties.Resources.AppIcon with actual 리소스 또는 Icon 파일 로드
+            _trayIcon = new NotifyIcon()
             {
-                _trayIcon.Visible = false;
-                _trayIcon.Dispose();
-            }
-        }
-        catch { }
+                Icon = Properties.Resources.AppIcon, // 대체: new Icon(Path.Combine(AppContext.BaseDirectory, "app.ico"))
+                Text = "YEJI-ON",
+                Visible = true,
+                ContextMenuStrip = BuildContextMenu()
+            };
 
-        try
-        {
-            _heartbeatWriter?.Dispose();
+            // 예외 처리 시 트레이 정리
+            Application.ApplicationExit += OnApplicationExit;
+            AppDomain.CurrentDomain.UnhandledException += (s, e) => OnApplicationExit(s, EventArgs.Empty);
+            Application.ThreadException += (s, e) => OnApplicationExit(s, EventArgs.Empty);
+            TaskScheduler.UnobservedTaskException += (s, e) => { OnApplicationExit(s, EventArgs.Empty); e.SetObserved(); };
         }
-        catch { }
+
+        private ContextMenuStrip BuildContextMenu()
+        {
+            var menu = new ContextMenuStrip();
+            menu.Items.Add("열기", null, (s, e) => ShowMainWindow());
+            menu.Items.Add("종료", null, (s, e) => ExitThread());
+            return menu;
+        }
+
+        private void ShowMainWindow()
+        {
+            // 필요 시 메인 윈도우를 띄우는 로직 추가
+        }
+
+        protected override void ExitThreadCore()
+        {
+            OnApplicationExit(this, EventArgs.Empty);
+            base.ExitThreadCore();
+        }
+
+        private void OnApplicationExit(object? sender, EventArgs e)
+        {
+            try
+            {
+                if (_trayIcon != null)
+                {
+                    _trayIcon.Visible = false;
+                    _trayIcon.Dispose();
+                }
+            }
+            catch { }
+
+            try
+            {
+                _heartbeatWriter?.Dispose();
+            }
+            catch { }
+        }      
     }
 }
